2d139deaff62321ae5467311ade77024d1f7cd0b
Sourabh Somani <sourabh_somani2010@hotmail.com>
2017-03-14 12:58:35 +0530

    Sever Side Validation Added

--- InterviewSite/Controllers/RegistrationController.cs
+++ InterviewSite/Controllers/RegistrationController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,20 +21,39 @@ namespace InterviewSite.Controllers
         [HttpPost]
         public string Index(User model)
         {
-            model.UserId = CommonFunctions.GetUserId();
-            model.RegistrationIpAddress = Request.UserHostAddress;
+            if (ValidateRegistrationData(model))
+            {
+                model.UserId = CommonFunctions.GetUserId();
+                model.RegistrationIpAddress = Request.UserHostAddress;
 
-            model.Password = Convert.ToBase64String(CommonFunctions.Encryption(model.Password,model.UserId));
+                model.Password = Convert.ToBase64String(CommonFunctions.Encryption(model.Password, model.UserId));
 
-            model.FacebookAccessToken = object.Equals(model.FacebookAccessToken, null) ? "" : model.FacebookAccessToken;
-            model.FacebookProfile = object.Equals(model.FacebookProfile, null) ? "" : model.FacebookProfile;
-            model.GoogleAccessToken = object.Equals(model.GoogleAccessToken, null) ? "" : model.GoogleAccessToken;
-            model.GoogleProfile = object.Equals(model.GoogleProfile, null) ? "" : model.GoogleProfile;
-            model.TwitterHandle = object.Equals(model.TwitterHandle, null) ? "" : model.TwitterHandle;
-            model.TwitterAccessToken = object.Equals(model.TwitterAccessToken, null) ? "" : model.TwitterAccessToken;
-            UserRepository userRepo = new UserRepository();
-            userRepo.SaveUser(model);
-            return "Successfully Registered";
+                model.FacebookAccessToken = object.Equals(model.FacebookAccessToken, null) ? "" : model.FacebookAccessToken;
+                model.FacebookProfile = object.Equals(model.FacebookProfile, null) ? "" : model.FacebookProfile;
+                model.GoogleAccessToken = object.Equals(model.GoogleAccessToken, null) ? "" : model.GoogleAccessToken;
+                model.GoogleProfile = object.Equals(model.GoogleProfile, null) ? "" : model.GoogleProfile;
+                model.TwitterHandle = object.Equals(model.TwitterHandle, null) ? "" : model.TwitterHandle;
+                model.TwitterAccessToken = object.Equals(model.TwitterAccessToken, null) ? "" : model.TwitterAccessToken;
+                UserRepository userRepo = new UserRepository();
+                userRepo.SaveUser(model);
+                return "Successfully Registered";
+            }
+            else
+            {
+                return "Something is wrong";
+            }
+        }
+
+        private bool ValidateRegistrationData(User model)
+        {
+            bool returnVal = true;
+            if (model.FirstName.Trim().Length == 0){returnVal = false;}
+            if (model.LastName.Trim().Length == 0) { returnVal = false; }
+            if (model.Email.Trim().Length == 0) { returnVal = false; }
+            bool IsEmailValid=Regex.IsMatch(model.Email.Trim(), @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+            if (!IsEmailValid) { returnVal = false; }
+            if (model.Password.Trim().Length == 0) { returnVal = false; }
+            return returnVal;
         }
     }
 }
