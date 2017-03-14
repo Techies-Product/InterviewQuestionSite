aa40bbf6bed3fd844998b0b3c2fcd410a0bc4d9b
Shaili <Shaili@SD-178>
2017-03-14 13:21:46 +0530

    Email already exist server side validation

--- InterviewSite/Controllers/RegistrationController.cs
+++ InterviewSite/Controllers/RegistrationController.cs
@@ -23,18 +23,21 @@ namespace InterviewSite.Controllers
         {
             if (ValidateRegistrationData(model))
             {
+                UserRepository userRepo = new UserRepository();
+                if (string.IsNullOrEmpty(userRepo.GetUserId(model.Email)))
+                {
+                    return "This Email Id Already Exists";
+                }
                 model.UserId = CommonFunctions.GetUserId();
                 model.RegistrationIpAddress = Request.UserHostAddress;
 
                 model.Password = Convert.ToBase64String(CommonFunctions.Encryption(model.Password, model.UserId));
-
                 model.FacebookAccessToken = object.Equals(model.FacebookAccessToken, null) ? "" : model.FacebookAccessToken;
                 model.FacebookProfile = object.Equals(model.FacebookProfile, null) ? "" : model.FacebookProfile;
                 model.GoogleAccessToken = object.Equals(model.GoogleAccessToken, null) ? "" : model.GoogleAccessToken;
                 model.GoogleProfile = object.Equals(model.GoogleProfile, null) ? "" : model.GoogleProfile;
                 model.TwitterHandle = object.Equals(model.TwitterHandle, null) ? "" : model.TwitterHandle;
                 model.TwitterAccessToken = object.Equals(model.TwitterAccessToken, null) ? "" : model.TwitterAccessToken;
-                UserRepository userRepo = new UserRepository();
                 userRepo.SaveUser(model);
                 return "Successfully Registered";
             }
